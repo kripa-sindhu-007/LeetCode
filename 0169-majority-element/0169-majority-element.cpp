44bf2e62df48404ca7a766276f1434ccb2ee5f24
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2024-05-26 07:07:46 +0530

    Time: 15 ms (77.71%), Space: 27.9 MB (12.44%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>map;
+
+        for(int it:nums){
+            map[it]++;
+        }
+        for(auto it:map){
+            if(it.second>(nums.size()/2)) return it.first;
+        }
+        return 0;
+        
+    }
+};
