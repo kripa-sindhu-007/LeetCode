622251cc6f83fb7d33593ea1a8398d2f47bf4995
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2024-06-01 08:06:29 +0530

    Time: 0 ms (100%), Space: 7.7 MB (78.98%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.cpp
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int score=0;
+        int score = 0;
 
-        for(int i=0;i<s.size()-1;i++){
-            score+=abs(int(s[i]-'0')-int(s[i+1]-'0'));
+        for (int i = 0; i < s.size() - 1; i++) {
+            score += abs(s[i] - s[i + 1]);
         }
         return score;
-        
     }
 };
