1e581f801f93b2d0abf397c5505a35b2ae055822
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2024-06-02 07:33:52 +0530

    Time: 19 ms (47.68%), Space: 26.9 MB (60.22%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int beg=0;
+        int end=s.size()-1;
+        while(beg<end)
+            swap(s[beg++],s[end--]);
+    }
+};
