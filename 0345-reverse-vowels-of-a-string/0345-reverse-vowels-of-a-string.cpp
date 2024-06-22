20eab552848d2718046f0400b3770175064a6ec6
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2024-06-22 08:41:04 +0530

    Time: 4 ms (81.85%), Space: 9 MB (95.33%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
+    bool isVowel(char ch) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            return true;
+        return false;
+    }
     string reverseVowels(string s) {
-        stack<char> st;
-        for (auto it : s) {
-            if (it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u' ||
-                it == 'A' || it == 'E' || it == 'I' || it == 'O' || it == 'U')
-                st.push(it);
+        int beg = 0;
+        int end = s.size() - 1;
+        while (beg < end) {
+            if (isVowel(s[beg]) && isVowel(s[end])) {
+                swap(s[beg], s[end]);
+                beg++;
+                end--;
+            } else if (!isVowel(s[beg]))
+                beg++;
+            else if (!isVowel(s[end]))
+                end--;
         }
-
-        string ans = "";
-        for (auto it : s) {
-            if (it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u' ||
-                it == 'A' || it == 'E' || it == 'I' || it == 'O' || it == 'U') {
-                char c = st.top();
-                ans += c;
-                st.pop();
-            } else
-                ans += it;
-        }
-        return ans;
+        return s;
     }
 };
