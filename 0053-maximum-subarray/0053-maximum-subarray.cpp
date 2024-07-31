5803dc646a6c0b6798786c58a6a5f2115cbd34bc
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2024-07-31 10:30:09 +0530

    Time: 84 ms (24.68%), Space: 70.5 MB (32.41%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int curr_sum=0;
-        int max_sum=INT_MIN;
-
-        //covering edge case when all element in the array is negative
-        int count=0;
-        for(int it:nums){
-            if(it>0) count++;
-            max_sum=max(max_sum,it);
+        int curr_sum = 0;
+        int max_sum = INT_MIN;
+        int neg_max = INT_MIN;
+        int count = 0;
+        for (int it : nums) {
+            curr_sum += it;
+            if (curr_sum < 0)
+                curr_sum = 0;
+            // edge case if all numbers in array are negative
+            if (it < 0) {
+                count++;
+                neg_max = max(neg_max, it);
+            }
+            max_sum = max(max_sum, curr_sum);
         }
-        if(count==0) return max_sum;
-        else max_sum=0;
-        
-
-        //works only when there is atleast 1 positive element in array
-        for(int it : nums){
-            curr_sum+=it;
-            if(curr_sum>=0) max_sum=max(max_sum,curr_sum);
-            else curr_sum=0;
-        }
-
-        return max_sum;
-        
+        return (count == nums.size()) ? neg_max : max_sum;
     }
 };
