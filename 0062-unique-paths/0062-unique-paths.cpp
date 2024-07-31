f4d18fc755505c038712537cc2d59b04c2b5fe00
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2024-07-31 09:49:10 +0530

    Time: 2 ms (42.83%), Space: 7.3 MB (84.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,14 @@
 class Solution {
-private:
-    int countpaths(int i, int j, int n, int m, vector<vector<int>> &dp) {
-        if (i == n-1 && j == m-1)
-            return 1;
-        else if (i >= n || j >= m)
-            return 0;
-        else if (dp[i][j] != -1)
-            return dp[i][j];
-        else
-            return dp[i][j] = countpaths(i + 1, j, n, m, dp) +
-                              countpaths(i, j + 1, n, m, dp);
-    }
-
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return countpaths(0, 0, m, n, dp);
+        int i = 0;
+        int j = 0;
+        int N = (m - i) + (n - j) - 2;
+        int r = m - i - 1;
+        double res = 1;
+        for (int i = 1; i <= r; i++) {
+            res = res * (N - r + i) / i;
+        }
+        return (int)res;
     }
 };
