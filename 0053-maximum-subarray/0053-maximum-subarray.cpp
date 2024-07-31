8d33bb5267b7d13d9bcfab1af0ee4523652df7d7
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2024-07-31 10:31:36 +0530

    Time: 66 ms (93.27%), Space: 70.6 MB (11.26%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,19 +3,12 @@ public:
     int maxSubArray(vector<int>& nums) {
         int curr_sum = 0;
         int max_sum = INT_MIN;
-        int neg_max = INT_MIN;
-        int count = 0;
         for (int it : nums) {
             curr_sum += it;
-            if (curr_sum < 0)
-                curr_sum = 0;
-            // edge case if all numbers in array are negative
-            if (it < 0) {
-                count++;
-                neg_max = max(neg_max, it);
-            }
             max_sum = max(max_sum, curr_sum);
+            if (curr_sum < 0)
+                curr_sum = 0;    
         }
-        return (count == nums.size()) ? neg_max : max_sum;
+        return max_sum;
     }
 };
