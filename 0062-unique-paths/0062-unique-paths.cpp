44682a3bc87b79e54f473e5653a9e06a2bbf4c0d
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2024-08-18 22:03:14 +0530

    Time: 0 ms (100%), Space: 7.5 MB (81.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = 1;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i > 0)
-                    dp[i][j] += dp[i - 1][j]; // from top
-                if (j > 0)
-                    dp[i][j] += dp[i][j - 1]; // from left
-            }
+        // vector<vector<int>> dp(m, vector<int>(n, 0));
+        // dp[0][0] = 1;
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (i > 0)
+        //             dp[i][j] += dp[i - 1][j]; // from top
+        //         if (j > 0)
+        //             dp[i][j] += dp[i][j - 1]; // from left
+        //     }
+        // }
+        // return dp[m - 1][n - 1];
+        int i = 0; // start coordinate
+        int j = 0; // start coordinate
+        int N = (m - i - 1) + (n - j - 1);
+        int r = m - i - 1;
+        double res = 1;
+        for (int i = 1; i <= r; i++) {
+            res = res * (N - r + i) / i;
         }
-        return dp[m - 1][n - 1];
+        return (int)res;
     }
 };
