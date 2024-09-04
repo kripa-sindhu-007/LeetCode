c3ec3060fc8c8e5c30f95fe595ad395a52fffa76
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2024-09-04 23:12:56 +0530

    Time: 109 ms (13.88%), Space: 86.1 MB (11.04%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
@@ -13,25 +13,45 @@
 class Solution {
 public:
     int goodNodes(TreeNode* root) {
-        if (!root)
+        // if (!root)
+        //     return 0;
+        // queue<pair<TreeNode*, int>> q;
+        // q.push({root, root->val});
+        // int goodNode = 0;
+        // while (!q.empty()) {
+        //     auto [node, max_val] = q.front();
+        //     q.pop();
+
+        //     if (max_val <= node->val)
+        //         goodNode++;
+
+        //     max_val = max(max_val, node->val);
+
+        //     if (node->left)
+        //         q.push({node->left, max_val});
+        //     if (node->right)
+        //         q.push({node->right, max_val});
+        // }
+        // return goodNode;
+
+        return dfs(root, INT_MIN);
+    }
+
+private:
+    int dfs(TreeNode* node, int curr_max) {
+        if (!node)
             return 0;
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, root->val});
-        int goodNode = 0;
-        while (!q.empty()) {
-            auto [node, max_val] = q.front();
-            q.pop();
 
-            if (max_val <= node->val)
-                goodNode++;
+        int good = 0;
 
-            max_val = max(max_val, node->val);
+        if (node->val >= curr_max)
+            good = 1;
 
-            if (node->left)
-                q.push({node->left, max_val});
-            if (node->right)
-                q.push({node->right, max_val});
-        }
-        return goodNode;
+        curr_max = max(curr_max, node->val);
+
+        good += dfs(node->left, curr_max);
+        good += dfs(node->right, curr_max);
+
+        return good;
     }
 };
