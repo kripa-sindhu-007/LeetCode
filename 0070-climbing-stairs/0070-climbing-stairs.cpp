6b026e6b84ce48ad4599cd711a723327a3cbfc94
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2024-10-02 22:32:49 +0530

    Time: 3 ms (29.69%), Space: 7.7 MB (37.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,17 @@
 class Solution {
-private:
-    int helper(int n,vector<int>& dp){
-        dp[0]=1;
-        dp[1]=1;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
-    }
 public:
     int climbStairs(int n) {
-        vector<int>dp(n+1,0);
-        return helper(n,dp);
-        
+        vector<int> dp(n+1, -1);
+        return helper(n, dp);
+    }
+
+    int helper(int n, vector<int>& dp) {
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
