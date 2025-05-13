0e4ec25c671b4e1907e45e2d0454a3f1ea88ea92
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2025-05-13 19:49:50 +0530

    Time: 2 ms (30.9%), Space: 52.4 MB (33.58%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        int pos = 0;
+        // int goal = nums.size() - 1;
 
-        int maxPos=0;
-
-        for(int i=0;i<nums.size();i++){
-            if(maxPos<i) return false;
-            maxPos=max(maxPos,i+nums[i]);
-
+        for (int i = 0; i < nums.size(); i++) {
+            if (pos < i)
+                return false;
+            pos = max(pos, nums[i] + i);
         }
         return true;
-        
     }
 };
