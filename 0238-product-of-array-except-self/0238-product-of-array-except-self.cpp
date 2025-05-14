0007de498b62e1b7310d99ba88daf3577955d02f
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2025-05-14 07:58:53 +0530

    Time: 3 ms (38.48%), Space: 40.3 MB (58.7%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,36 +3,30 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans(n, 1);
-        vector<int> left_prd(n, 1);
-        vector<int> right_prd(n, 1);
 
         for (int i = 0; i < n; i++) {
             if (i == 0) {
-                left_prd[i] = nums[i];
+                ans[i] = nums[i];
                 continue;
             }
-            left_prd[i] = left_prd[i - 1] * nums[i];
+            ans[i] = ans[i - 1] * nums[i];
         }
+
+        int right_prd = nums[n - 1];
 
         for (int i = n - 1; i >= 0; i--) {
             if (i == n - 1) {
-                right_prd[i] = nums[i];
+                ans[i] = ans[i - 1];
                 continue;
             }
-            right_prd[i] = right_prd[i + 1] * nums[i];
+            if (i == 0) {
+                ans[i] = right_prd;
+                continue;
+            }
+            ans[i] = ans[i - 1] * right_prd;
+            right_prd *= nums[i];
         }
 
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                ans[i] = right_prd[i + 1];
-                continue;
-            }
-            if (i == n - 1) {
-                ans[i] = left_prd[i - 1];
-                continue;
-            }
-            ans[i] = left_prd[i - 1] * right_prd[i + 1];
-        }
         return ans;
     }
 };
