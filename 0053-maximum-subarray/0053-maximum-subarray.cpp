dcef6152f151dc7fd8d9997ed846fba6f7e3ae6d
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2025-05-19 21:04:39 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.44%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,11 +3,12 @@ public:
     int maxSubArray(vector<int>& nums) {
         int curr_sum = 0;
         int max_sum = INT_MIN;
-        for (int it : nums) {
-            curr_sum += it;
-            max_sum = max(max_sum, curr_sum);
+
+        for (int i = 0; i < nums.size(); i++) {
             if (curr_sum < 0)
-                curr_sum = 0;    
+                curr_sum = 0;
+            curr_sum += nums[i];
+            max_sum = max(max_sum, curr_sum);
         }
         return max_sum;
     }
