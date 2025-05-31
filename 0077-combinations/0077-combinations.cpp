26d3194283679b2ff098932f4ce5d1775624236f
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2025-05-31 07:24:03 +0530

    Time: 40 ms (52.02%), Space: 62.4 MB (95.3%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,23 +1,25 @@
 class Solution {
-public:
-    vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> ans;
-        vector<int> curr_combi;
-        helper(n, k, ans, curr_combi, 1);
-        return ans;
-    }
 private:
-    void helper (int n, int k, vector<vector<int>>& ans, vector<int>& curr_combi,
-         int first) {
-
-        if (curr_combi.size() == k) {
-            ans.push_back(curr_combi);
+    void backtrack(int n, int k, vector<vector<int>>& all_combination,
+                   vector<int>& curr_combination, int first) {
+        if (curr_combination.size() == k) {
+            all_combination.push_back(curr_combination);
             return;
         }
+
         for (int i = first; i <= n; i++) {
-            curr_combi.push_back(i);              // choose
-            helper(n, k, ans, curr_combi, i + 1); // explore
-            curr_combi.pop_back();                // backtrack
+            curr_combination.push_back(i);
+            backtrack(n, k, all_combination, curr_combination, i + 1);
+            curr_combination.pop_back();
         }
+    }
+
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> all_combination;
+        vector<int> curr_combination;
+        int first = 1;
+        backtrack(n, k, all_combination, curr_combination, first);
+        return all_combination;
     }
 };
