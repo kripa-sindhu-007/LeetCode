7a56e0654cea9d567fe7edc49391e1066f0e3fab
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2025-06-02 07:31:24 +0530

    Time: 4 ms (36.62%), Space: 15.5 MB (70.99%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,18 +1,26 @@
 class Solution {
-public:
-    void helper(int n, vector<string>& ans, string temp, int start, int end) {
-        if (temp.size() == (2 * n)) {
-            ans.push_back(temp);
+private:
+    void backtrack(vector<string>& all_combi, int n, int open, int close,
+                   string curr_combi) {
+        if (curr_combi.length() == 2 * n) {
+            all_combi.push_back(curr_combi);
             return;
         }
-        if (start < n)
-            helper(n, ans, temp + '(', start + 1, end);
-        if (end < start)
-            helper(n, ans, temp + ')', start, end + 1);
+        if (open < n) {
+            backtrack(all_combi, n, open + 1, close, curr_combi + '(');
+        }
+        if (close < open) {
+            backtrack(all_combi, n, open, close + 1, curr_combi + ')');
+        }
     }
+
+public:
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        helper(n, ans, "", 0, 0);
-        return ans;
+        vector<string> all_combi;
+        int open = 0;
+        int close = 0;
+        string curr_combi = "";
+        backtrack(all_combi, n, open, close, curr_combi);
+        return all_combi;
     }
 };
