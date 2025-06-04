136c5fc40b51ce68bd011fafc4473b9836e98f81
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2025-06-04 07:24:41 +0530

    Time: 0 ms (100%), Space: 12.7 MB (77.7%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,10 +15,10 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if (!p && !q)
             return true;
-        if (!p && q)
+        if (!p || !q)
             return false;
-        if (p && !q)
-            return false;
+        // if (p && !q)
+        //     return false;
         if (p->val != q->val)
             return false;
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
