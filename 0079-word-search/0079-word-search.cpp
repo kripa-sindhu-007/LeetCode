be968ea781dffe426d1803f305e7793c0a61b16d
Kripa Sindhu <152695457+kripa-sindhu-007@users.noreply.github.com>
2025-06-05 07:53:24 +0530

    Time: 830 ms (26.6%), Space: 11.6 MB (36.31%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,25 +1,22 @@
 class Solution {
 private:
-    bool helper(int i, int j, vector<vector<char>>& board, string& word,
+    bool helper(int i, int j, vector<vector<char>>& board, string word,
                 int idx) {
-
         if (idx == word.size())
             return true;
-
         if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() ||
             board[i][j] != word[idx])
             return false;
 
         char temp = board[i][j];
-        board[i][j] = '&';
+        board[i][j] = '$';
 
-        bool decision = helper(i + 1, j, board, word, idx + 1) ||
-                        helper(i, j + 1, board, word, idx + 1) ||
-                        helper(i - 1, j, board, word, idx + 1) ||
-                        helper(i, j - 1, board, word, idx + 1);
-
+        bool descision = helper(i, j + 1, board, word, idx + 1) ||
+                         helper(i + 1, j, board, word, idx + 1) ||
+                         helper(i - 1, j, board, word, idx + 1) ||
+                         helper(i, j - 1, board, word, idx + 1);
         board[i][j] = temp;
-        return decision;
+        return descision;
     }
 
 public:
@@ -30,7 +27,6 @@ public:
                     return true;
             }
         }
-
         return false;
     }
 };
